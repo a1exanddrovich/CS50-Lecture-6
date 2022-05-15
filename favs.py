e58c69be57d7dd32a30a55a1700f2c0aef02d4ee
Alexey Bliznichenko <l.bliznichenko@mail.ru>
2022-05-15 11:47:37 +0300

    Regular expressions used and search for count by title added

--- favs.py
+++ favs.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 # see data from csv
 titles = set()
@@ -33,5 +34,26 @@ for title in sorted(titles, key=lambda title: titles[title], reverse=True):
     print(title)
 
 
+# Cleaning data using simple regular expressions
+counter = 0
 
+with open("favs.csv", "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        title = row["title"].strip().upper()
+        if re.search("^(OFFICE|THE.OFFICE)$", title):
+            counter += 1
 
+print(f"Number of people who like the office is {counter}")
+
+# ask user for the title
+
+title = input("Title: ").strip().upper()
+
+counter = 0
+
+with open("favs.csv", "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        if row["title"].strip().upper() == title:
+            counter += 1
